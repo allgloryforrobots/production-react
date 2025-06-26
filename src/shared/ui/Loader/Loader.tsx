52b28b7393d00f2855de4b13921a3c792e37a48d
Илья Булатов <ilia.bulatov@infoguild.ru>
2025-06-26 17:19:49 +0300

    add Loader cmp, not found page

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import './Loader.scss';
+
+export const Loader: FC = () => (
+    <div className="loadingio-spinner-rolling">
+        <div className="ldio">
+            <div />
+        </div>
+    </div>
+);
---
+++ src/shared/ui/PageLoader/PageLoader.tsx
@@ -0,0 +1,20 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { FC } from 'react';
+import cls from './PageLoader.module.scss';
+import { Loader } from '../Loader/Loader';
+
+interface PageLoaderProps {
+    className?: string
+}
+
+export const PageLoader: FC<PageLoaderProps> = (props) => {
+    const {
+        className,
+    } = props;
+
+    return (
+        <div className={classNames(cls.PageLoader, {}, [className])}>
+            <Loader />
+        </div>
+    );
+};
