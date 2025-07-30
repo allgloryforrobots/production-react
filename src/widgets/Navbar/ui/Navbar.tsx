559604e0e7c6bc9add08137733116ceaf392aee2
Илья Булатов <ilia.bulatov@infoguild.ru>
2025-07-30 10:54:23 +0300

    add modal, portal, change header

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,14 +1,36 @@
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.navbar, {}, [className])}>
-        <div className={cls.links}>
-            /
+export const Navbar = ({ className }: NavbarProps) => {
+    const [isAuthModal, setIsAuthModal] = useState(false);
+    const { t } = useTranslation();
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
+
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+
+            {/* eslint-disable-next-line i18next/no-literal-string */ }
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                Lore
+            </Modal>
         </div>
-    </div>
-);
+    );
+};
